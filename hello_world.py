9242025ae9d844fb84d54a3523ba519ca9a55f8d
MicroBrew09 <64977129+MicroBrew09@users.noreply.github.com>
2021-01-22 13:00:57 -0800

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,8 +1,12 @@
 import random
+import numpy
 
 print("Welcome to my CMPT765 exercise 2")
 print("GITHUB Organizations is interesting!")
 
 num = random.randint(1,9)
+num1 = np.random.randn(1,2)
+
 print("Here's a random number:", num)
+print("Here is a numpy random number:",num1)
 
